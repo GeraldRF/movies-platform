531f61c321c962aa5729ed567aac26507f95dd79
Gerald Ramírez <geral.rafo@gmail.com>
2022-05-29 05:54:23 -0600

    se cambio vue config

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,13 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  
-
+  transpileDependencies: true,
+  devServer: { allowedHosts: 'all', },
+  pluginOptions: {
+    moment: {
+      locales: [
+        ''
+      ]
+    }
+  }
 });
